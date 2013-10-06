3ccbb4c92a6e4c8315c255571a2b1b4029812c8c
Raul Martinez Padilla <raul@deceroainfinito.es>
2013-10-07 00:49:22 +0200

    Adding defuns, symbols and keys to init file

--- init.el
+++ init.el
@@ -4,6 +4,12 @@
 
 ;; ******************************  Loads primary configurartions and paths location
 (add-to-list 'load-path (concat user-emacs-directory "config"))
+;; ******************************  Loads defuns
+(setq defuns-dir (expand-file-name "defuns" user-emacs-directory))
+(dolist (file (directory-files defuns-dir t "\\w+"))
+  (when (file-regular-p file)
+    (load file)))
+
 
 ;; ******************************  Misc
 (require 'misc)
@@ -15,12 +21,16 @@
 (require 'packages)
 
 
+
 ;; ******************************  Mac
 (setq is-mac (equal system-type 'darwin))
 (when is-mac (require 'mac))
 
-
-
+;; ******************************  Mac
+(require `keys)
 
 ;; ******************************  eshell
 (require 'e_shell)
+
+;; ******************************  Symbols
+(require 'symbols)
